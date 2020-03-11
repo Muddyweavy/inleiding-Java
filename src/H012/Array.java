5f326ebe2583249cb5f406cea7d524030f385f1f
Muddyweavy <54936267+Muddyweavy@users.noreply.github.com>
2020-03-11 17:21:38 +0100

    Add files via upload

--- src/H012/Array.java
+++ src/H012/Array.java
@@ -5,6 +5,7 @@ import java.applet.*;
 
 public class Array extends Applet {
     double[] array;
+    int uitkomst;
 
     public void init() {
         array = new double[10];
@@ -18,10 +19,12 @@ public class Array extends Applet {
         array[7] = 800;
         array[8] = 900;
         array[9] = 1000;
+        uitkomst = (int) ((array[0] + array[1] + array[2] + array[3] + array[4] + array[5] + array[6] + array[7] + array[8] + array[9]) / 10);
     }
 
     public void paint(Graphics g) {
         for (int teller = 0; teller < array.length; teller ++)
             g.drawString("" + array[teller], 50, 20 * teller + 20);
+            g.drawString("gemiddelde =" + uitkomst, 90, 20);
         }
     }
--- src/H012/ButtonArray.java
+++ src/H012/ButtonArray.java
@@ -4,24 +4,40 @@ import java.awt.*;
 import java.applet.*;
 
 public class ButtonArray extends Applet {
-    double[] array;
+    //double[] array;
 
     public void init() {
-        array = new double[10];
-        array[0] = 100;
-        array[1] = 200;
-        array[2] = 300;
-        array[3] = 400;
-        array[4] = 500;
-        array[5] = 600;
-        array[6] = 700;
-        array[7] = 800;
-        array[8] = 900;
-        array[9] = 1000;
+ //       array = new double[25];
+//        array[0] = new Button();
+    //    array[1] = new Button();
+       // array[2] = new Button();
+//        array[3] = new Button();
+  //      array[4] = new Button();
+    //    array[5] = new Button();
+      //  array[6] = new Button();
+        //array[7] = new Button();
+//        array[8] = new Button();
+  //      array[9] = new Button();
+    //    array[10] = new Button();
+      //  array[11] = new Button();
+        //array[12] = new Button();
+        //array[13] = new Button();
+//        array[14] = new Button();
+  //      array[15] = new Button();
+    //    array[16] = new Button();
+      //  array[17] = new Button();
+        //array[18] = new Button();
+//        array[19] = new Button();
+  //      array[20] = new Button();
+    //    array[21] = new Button();
+      //  array[22] = new Button();
+        //array[23] = new Button();
+        //array[24] = new Button();
+   //     array[25] = new Button();
     }
 
     public void paint(Graphics g) {
-        for (int teller = 0; teller < array.length; teller ++)
-            g.drawString("" + array[teller], 50, 20 * teller + 20);
+  //      for (int teller = 0; teller < array.length; teller ++)
+   //         g.drawString("" + array[teller], 50, 20 * teller + 20);
     }
 }
